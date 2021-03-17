59dd3bd6697c3c4638fafc4ee3a35e00a522a02c
Smart <kwesi.smart@turntabl.io>
2021-03-17 09:28:42 +0000

    Replace homepage with login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePageComponent } from './components/home-page/home-page.component';
+// import { HomePageComponent } from './components/home-page/home-page.component';
 import { LoginComponent } from './components/login/login.component';
 
 
 const routes: Routes = [
-  { path: '', component: HomePageComponent },
-  { path: 'login', component: LoginComponent },
+  { path: '', component: LoginComponent },
+  // { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
