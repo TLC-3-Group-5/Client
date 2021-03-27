beb3c99312562879945bfacaad93190c89c1fd63
Stella Dare <stelladare2@gmail.com>
2021-03-27 18:26:26 +0000

    Create and style homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-// import { HomePageComponent } from './components/home-page/home-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 import { LoginComponent } from './components/login/login.component';
 
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
+  { path: '', component: HomePageComponent },
+  // { path: '', component: LoginComponent }  
   // { path: 'login', component: LoginComponent },
 ];
 
